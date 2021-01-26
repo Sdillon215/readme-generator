a391e624d848779e8a409c8900317e1eb0768a0b
Sean <sdillon215@gmail.com>
2021-01-25 18:33:54 -0700

    user input added to answers object

--- index.js
+++ index.js
@@ -34,8 +34,31 @@ const questions = () => {
             type: 'input',
             name: 'link',
             message: 'If your application is deployed please provide the link!'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions and examples for use. Add screenshots as neccessary.'
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'If there are collaborators provide their github account.'
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Add your license.'
         }
-    ]);
+    ])
+    .then(answers => {
+        console.log(answers);
+    })
 };
 
 // TODO: Create a function to write README file
